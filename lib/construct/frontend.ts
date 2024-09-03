a84566c10e326819a86469b9dfe4d41c76667e55
KIM JONG IL <124699796+stoneidev@users.noreply.github.com>
2024-09-03 09:54:48 +0900

    Update frontend.ts
    20240903 amplify repo change

--- lib/construct/frontend.ts
+++ lib/construct/frontend.ts
@@ -38,7 +38,7 @@ export class FrontendConstruct extends Construct {
     const amplifyApp = new amplify.CfnApp(this, "AmplifyL3App", {
       name: "stonei-web",
       accessToken: cdk.SecretValue.secretsManager("GithubToken").unsafeUnwrap(),
-      repository: "https://github.com/stoneidev/serverless-admin",
+      repository: "https://github.com/lg-ensol/cdk",
       platform: "WEB_COMPUTE",
       iamServiceRole: amplifyRole.roleArn,
       environmentVariables: [
@@ -93,6 +93,6 @@ export class FrontendConstruct extends Construct {
     });
 
     this.appId = amplifyApp.attrAppId;
-    this.repoUrl = "https://github.com/stoneidev/serverless-admin";
+    this.repoUrl = "https://github.com/lg-ensol/cdk";
   }
 }
